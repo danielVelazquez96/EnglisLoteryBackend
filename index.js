1a09bb23beb5ae1b28e7b333e8e6427783f10d08
daniel Velazquez <daniel_velazquez96@outlook.es>
2022-03-21 17:52:42 -0600

    se realizo el moduclo de cloudinary y el endpoint para subir las imagenes a cloudinary

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// Cloudinary
+import  cloudinary  from './utils/cloudinary.js';
+
 import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+
+
 // const path=require('path');
 
 const app=express();
@@ -34,12 +40,30 @@ const Lotery=mongoose.model("version1",{number: Number,name: String,url: String}
 
 // endpoint get
 app.get('/getData',(req,res)=>{
+    
     Lotery.find()
         .then(doc=>{
             res.json(doc);
         })
         .catch(err=>console.log(`Failed to download data err: ${err}`));
 })
+
+app.post('/api/upload', async (req, res) => {
+    console.log("entro");
+    try {
+        const fileStr = req.body.data;
+        const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+            englishLoteryPublic: 'englishLoteryPublic',
+        });
+        console.log(uploadResponse);
+        res.json({ msg: 'yaya' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ err: 'Something went wrong' });
+    }   
+});
+
+
 
 app.get('/',(req,res)=>{
     res.json('Hello, API works');
